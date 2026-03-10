b1eb6f5f7658135ee042fdd7e50f386782256e64
Ruben <81467803+Rubino-tech@users.noreply.github.com>
2026-03-10 14:26:52 +0100

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCbtJp83pLB2bdnklJ3T1aKfKNpDu0ILzc",
+  authDomain: "rubensticker-rs67.firebaseapp.com",
+  projectId: "rubensticker-rs67",
+  storageBucket: "rubensticker-rs67.firebasestorage.app",
+  messagingSenderId: "556124047873",
+  appId: "1:556124047873:web:3ad74c961be197c4158e00"
+};
