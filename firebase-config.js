6f6ad500f10e046554660dacc372ead35f52ea9c
Rubino-tech <Rubino-tech@users.noreply.github.com>
2026-03-15 12:11:45 +0000

    deploy: 9d668ba9ed83175a223bfb25868d75ef6024eb3c

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA5Q0j24ydVXAXxTnTQzBu0nmPwDgLIL2g",
+  authDomain: "rubensticker-rs67.firebaseapp.com",
+  projectId: "rubensticker-rs67",
+  storageBucket: "rubensticker-rs67.firebasestorage.app",
+  messagingSenderId: "556124047873",
+  appId: "1:556124047873:web:3ad74c961be197c4158e00"
+};
